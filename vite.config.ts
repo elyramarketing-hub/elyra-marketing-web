f31b1dfcc753bbb3661b1cbf4e15408621ed6d2d
elyramarketinghub <elyramarketinghub@gmail.com>
2025-09-01 21:25:03 -0700

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.');
+
+  return {
+    base: '/elyra-ssm/',   // 👈 must match your GitHub repo name
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
+});
